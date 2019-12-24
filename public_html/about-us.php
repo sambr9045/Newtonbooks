ae7297f4ebc70038b15d147ae6f1ec9acb41ff9d
Samrob90 <bukarishamsu8@gmail.com>
2019-12-24 12:08:02 +0100

    23 december 2019

---
+++ public_html/about-us.php
@@ -0,0 +1,222 @@
+﻿<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>About | Books Library eCommerce Store</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assets/images/icon.png">
+
+	<!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800"
+		rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+	<!-- Stylesheets -->
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/plugins.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+	<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+
+</head>
+
+<body>
+	<!--[if lte IE 9]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+	<![endif]-->
+
+	<!-- Main wrapper -->
+	<div class="wrapper" id="wrapper">
+		
+			<?php include("include/header.php") ?>
+	   
+		<!-- Start Bradcaump area -->
+		<div class="ht__bradcaump__area bg-image--3">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="bradcaump__inner text-center">
+							<h2 class="bradcaump-title">About us</h2>
+							<nav class="bradcaump-content">
+								<a class="breadcrumb_item" href="index.html">Home</a>
+								<span class="brd-separetor">/</span>
+								<span class="breadcrumb_item active">About us</span>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Bradcaump area -->
+		<!-- Start About Area -->
+		<div class="page-about about_area bg--white section-padding--lg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="section__title--3 text-center pb--30">
+							<h2>Our Process Skill Of High</h2>
+							<p>the right people for your project</p>
+						</div>
+					</div>
+				</div>
+				<div class="row align-items-center">
+					<div class="col-lg-6 col-sm-12 col-12">
+						<div class="content text-left text_style--2">
+							<h2>we have skills to show</h2>
+							<div class="skill-container">
+								<!-- Start single skill -->
+								<div class="single-skill">
+									<p>Customer Favorites</p>
+									<div class="progress">
+										<div class="progress-bar wow fadeInLeft" data-wow-duration="0.8s"
+											data-wow-delay=".5s" role="progressbar" aria-valuenow="90" aria-valuemin="0"
+											aria-valuemax="100" style="width:90%"><span class="pen-lable"></span>
+										</div>
+									</div>
+								</div>
+								<!-- End single skill -->
+								<!-- Start single skill -->
+								<div class="single-skill">
+									<p>Popular Authors</p>
+									<div class="progress">
+										<div class="progress-bar wow fadeInLeft" data-wow-duration="0.8s"
+											data-wow-delay=".5s" role="progressbar" aria-valuenow="95" aria-valuemin="0"
+											aria-valuemax="100" style="width:95%"><span class="pen-lable"></span>
+										</div>
+									</div>
+								</div>
+								<!-- End single skill -->
+								<!-- Start single skill -->
+								<div class="single-skill">
+									<p>Bestselling Series</p>
+									<div class="progress">
+										<div class="progress-bar wow fadeInLeft" data-wow-duration="0.8s"
+											data-wow-delay=".5s" role="progressbar" aria-valuenow="93" aria-valuemin="0"
+											aria-valuemax="100" style="width:93%"><span class="pen-lable"></span>
+										</div>
+									</div>
+								</div>
+								<!-- End single skill -->
+								<!-- Start single skill -->
+								<div class="single-skill">
+									<p>Bargain Books</p>
+									<div class="progress">
+										<div class="progress-bar wow fadeInLeft" data-wow-duration="0.8s"
+											data-wow-delay=".5s" role="progressbar" aria-valuenow="90" aria-valuemin="0"
+											aria-valuemax="100" style="width:90%"><span class="pen-lable"></span>
+										</div>
+									</div>
+								</div>
+								<!-- End single skill -->
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-sm-12 col-12">
+						<div class="content">
+							<h3>Buy Book</h3>
+							<h2>Different Knowledge</h2>
+							<p class="mt--20 mb--20">Claritas est etiam processus dynamicus, qui sequitur mutationem
+								consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum
+								claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta
+								decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in
+								futurum.</p>
+							<strong>London Address</strong>
+							<p>34 Parer Place via Musk Avenue Kelvin Grove, QLD, 4059</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End About Area -->
+		<!-- Start Team Area -->
+		<section class="wn__team__area pb--75 bg--white">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="section__title--3 text-center">
+							<h2>Meet our team of experts</h2>
+							<p>the right people for your project</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<!-- Start Single Team -->
+					<div class="col-lg-4 col-md-4 col-sm-6 col-12">
+						<div class="wn__team">
+							<div class="thumb">
+								<img src="assets/images/about/team/1.jpg" alt="Team images">
+							</div>
+							<div class="content text-center">
+								<h4>JOHN SMITH</h4>
+								<p>Manager</p>
+								<ul class="team__social__net">
+									<li><a href="#"><i class="icon-social-twitter icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-tumblr icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-facebook icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-linkedin icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-pinterest icons"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- End Single Team -->
+					<!-- Start Single Team -->
+					<div class="col-lg-4 col-md-4 col-sm-6 col-12">
+						<div class="wn__team">
+							<div class="thumb">
+								<img src="assets/images/about/team/2.jpg" alt="Team images">
+							</div>
+							<div class="content text-center">
+								<h4>ALICE KIM</h4>
+								<p>Co-Founder</p>
+								<ul class="team__social__net">
+									<li><a href="#"><i class="icon-social-twitter icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-tumblr icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-facebook icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-linkedin icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-pinterest icons"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- End Single Team -->
+					<!-- Start Single Team -->
+					<div class="col-lg-4 col-md-4 col-sm-6 col-12">
+						<div class="wn__team">
+							<div class="thumb">
+								<img src="assets/images/about/team/3.jpg" alt="Team images">
+							</div>
+							<div class="content text-center">
+								<h4>VICTORIA DOE</h4>
+								<p>Marketer</p>
+								<ul class="team__social__net">
+									<li><a href="#"><i class="icon-social-twitter icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-tumblr icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-facebook icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-linkedin icons"></i></a></li>
+									<li><a href="#"><i class="icon-social-pinterest icons"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- End Single Team -->
+				</div>
+			</div>
+		</section>
+		<!-- End Team Area -->
+
+	</div>
+	<!-- //Main wrapper -->
+
+	<?php include("include/footer.php") ?>
+
+</body>
+</html>
---
+++ public_html/include/header.php
@@ -0,0 +1,302 @@
+		<!-- Header -->
+		<header id="wn__header" class="oth-page header__area header__absolute sticky__header">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-4 col-sm-4 col-7 col-lg-2">
+						<div class="logo">
+							<a href="index.html">
+								<img src="assets/images/logo/logo.png" alt="logo images">
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-8 d-none d-lg-block">
+                    <nav class="mainmenu__nav">
+							<ul class="meninmenu d-flex justify-content-start">
+								<li class="drop with--one--item"><a href="index">Home</a>
+									
+								</li>
+								<li class="drop"><a href="shop">Shop</a>
+								
+								</li>
+								<li class="drop"><a href="shop-grid.html">Books</a>
+									<div class="megamenu mega03">
+										<ul class="item item03">
+											<li class="title">Categories</li>
+											<li><a href="shop-grid.html">Biography </a></li>
+											<li><a href="shop-grid.html">Business </a></li>
+											<li><a href="shop-grid.html">Cookbooks </a></li>
+											<li><a href="shop-grid.html">Health & Fitness </a></li>
+											<li><a href="shop-grid.html">History </a></li>
+										</ul>
+										<ul class="item item03">
+											<li class="title">Customer Favourite</li>
+											<li><a href="shop-grid.html">Mystery</a></li>
+											<li><a href="shop-grid.html">Religion & Inspiration</a></li>
+											<li><a href="shop-grid.html">Romance</a></li>
+											<li><a href="shop-grid.html">Fiction/Fantasy</a></li>
+											<li><a href="shop-grid.html">Sleeveless</a></li>
+										</ul>
+										<ul class="item item03">
+											<li class="title">Collections</li>
+											<li><a href="shop-grid.html">Science </a></li>
+											<li><a href="shop-grid.html">Fiction/Fantasy</a></li>
+											<li><a href="shop-grid.html">Self-Improvemen</a></li>
+											<li><a href="shop-grid.html">Home & Garden</a></li>
+											<li><a href="shop-grid.html">Humor Books</a></li>
+										</ul>
+									</div>
+								</li>
+								
+								<li class="drop"><a href="about-us">About us</a>
+								
+								</li>
+								<li class="drop"><a href="blog">Blog</a>
+									
+								</li>
+								<li><a href="contact-us">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="col-md-8 col-sm-8 col-5 col-lg-2">
+						<ul class="header__sidebar__right d-flex justify-content-end align-items-center">
+							<li class="shop_search"><a class="search__active" href="#"></a></li>
+							<li class="wishlist"><a href="#"></a></li>
+							<li class="shopcart"><a class="cartbox_active" href="#"><span
+										class="product_qun">3</span></a>
+								<!-- Start Shopping Cart -->
+								<div class="block-minicart minicart__active">
+									<div class="minicart-content-wrapper">
+										<div class="micart__close">
+											<span>close</span>
+										</div>
+										<div class="items-total d-flex justify-content-between">
+											<span>3 items</span>
+											<span>Cart Subtotal</span>
+										</div>
+										<div class="total_amount text-right">
+											<span>$66.00</span>
+										</div>
+										<div class="mini_action checkout">
+											<a class="checkout__btn" href="cart.html">Go to Checkout</a>
+										</div>
+										<div class="single__items">
+											<div class="miniproduct">
+												<div class="item01 d-flex">
+													<div class="thumb">
+														<a href="product-details.html"><img
+																src="assets/images/product/sm-img/1.jpg"
+																alt="product images"></a>
+													</div>
+													<div class="content">
+														<h6><a href="product-details.html">Voyage Yoga Bag</a></h6>
+														<span class="prize">$30.00</span>
+														<div class="product_prize d-flex justify-content-between">
+															<span class="qun">Qty: 01</span>
+															<ul class="d-flex justify-content-end">
+																<li><a href="#"><i class="zmdi zmdi-settings"></i></a>
+																</li>
+																<li><a href="#"><i class="zmdi zmdi-delete"></i></a>
+																</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+												<div class="item01 d-flex mt--20">
+													<div class="thumb">
+														<a href="product-details.html"><img
+																src="assets/images/product/sm-img/3.jpg"
+																alt="product images"></a>
+													</div>
+													<div class="content">
+														<h6><a href="product-details.html">Impulse Duffle</a></h6>
+														<span class="prize">$40.00</span>
+														<div class="product_prize d-flex justify-content-between">
+															<span class="qun">Qty: 03</span>
+															<ul class="d-flex justify-content-end">
+																<li><a href="#"><i class="zmdi zmdi-settings"></i></a>
+																</li>
+																<li><a href="#"><i class="zmdi zmdi-delete"></i></a>
+																</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+												<div class="item01 d-flex mt--20">
+													<div class="thumb">
+														<a href="product-details.html"><img
+																src="assets/images/product/sm-img/2.jpg"
+																alt="product images"></a>
+													</div>
+													<div class="content">
+														<h6><a href="product-details.html">Compete Track Tote</a></h6>
+														<span class="prize">$40.00</span>
+														<div class="product_prize d-flex justify-content-between">
+															<span class="qun">Qty: 03</span>
+															<ul class="d-flex justify-content-end">
+																<li><a href="#"><i class="zmdi zmdi-settings"></i></a>
+																</li>
+																<li><a href="#"><i class="zmdi zmdi-delete"></i></a>
+																</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="mini_action cart">
+											<a class="cart__btn" href="cart.html">View and edit cart</a>
+										</div>
+									</div>
+								</div>
+								<!-- End Shopping Cart -->
+							</li>
+							<li class="setting__bar__icon"><a class="setting__active" href="#"></a>
+								<div class="searchbar__content setting__block">
+									<div class="content-inner">
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>Currency</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<span class="currency-trigger">USD - US Dollar</span>
+													<ul class="switcher-dropdown">
+														<li>GBP - British Pound Sterling</li>
+														<li>EUR - Euro</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>Language</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<span class="currency-trigger">English01</span>
+													<ul class="switcher-dropdown">
+														<li>English02</li>
+														<li>English03</li>
+														<li>English04</li>
+														<li>English05</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>Select Store</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<span class="currency-trigger">Fashion Store</span>
+													<ul class="switcher-dropdown">
+														<li>Furniture</li>
+														<li>Shoes</li>
+														<li>Speaker Store</li>
+														<li>Furniture</li>
+													</ul>
+												</div>
+											</div>
+										</div>
+										<div class="switcher-currency">
+											<strong class="label switcher-label">
+												<span>My Account</span>
+											</strong>
+											<div class="switcher-options">
+												<div class="switcher-currency-trigger">
+													<div class="setting__menu">
+														<span><a href="#">Compare Product</a></span>
+														<span><a href="#">My Account</a></span>
+														<span><a href="#">My Wishlist</a></span>
+														<span><a href="#">Sign In</a></span>
+														<span><a href="#">Create An Account</a></span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- Start Mobile Menu -->
+				<div class="row d-none">
+					<div class="col-lg-12 d-none">
+						<nav class="mobilemenu__nav">
+							<ul class="meninmenu">
+								<li><a href="index.html">Home</a>
+									<ul>
+										<li><a href="index.html">Home Style Default</a></li>
+										<li><a href="index-2.html">Home Style Two</a></li>
+										<li><a href="index-3.html">Home Style Three</a></li>
+										<li><a href="index-box.html">Home Box Style</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Pages</a>
+									<ul>
+										<li><a href="about.html">About Page</a></li>
+										<li><a href="portfolio.html">Portfolio</a>
+											<ul>
+												<li><a href="portfolio.html">Portfolio</a></li>
+												<li><a href="portfolio-three-column.html">Portfolio 3 Column</a></li>
+												<li><a href="portfolio-details.html">Portfolio Details</a></li>
+											</ul>
+										</li>
+										<li><a href="my-account.html">My Account</a></li>
+										<li><a href="cart.html">Cart Page</a></li>
+										<li><a href="checkout.html">Checkout Page</a></li>
+										<li><a href="wishlist.html">Wishlist Page</a></li>
+										<li><a href="error404.html">404 Page</a></li>
+										<li><a href="faq.html">Faq Page</a></li>
+										<li><a href="team.html">Team Page</a></li>
+									</ul>
+								</li>
+								<li><a href="shop-grid.html">Shop</a>
+									<ul>
+										<li><a href="shop-grid.html">Shop Grid</a></li>
+										<li><a href="shop-list.html">Shop List</a></li>
+										<li><a href="shop-left-sidebar.html">Shop Left Sidebar</a></li>
+										<li><a href="shop-right-sidebar.html">Shop Right Sidebar</a></li>
+										<li><a href="shop-no-sidebar.html">Shop No sidebar</a></li>
+										<li><a href="single-product.html">Single Product</a></li>
+									</ul>
+								</li>
+								<li><a href="blog.html">Blog</a>
+									<ul>
+										<li><a href="blog.html">Blog Page</a></li>
+										<li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+										<li><a href="blog-no-sidebar.html">Blog No Sidebar</a></li>
+										<li><a href="blog-details.html">Blog Details</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<!-- End Mobile Menu -->
+				<div class="mobile-menu d-block d-lg-none">
+				</div>
+				<!-- Mobile Menu -->
+			</div>
+		</header>
+        <!-- //Header -->
+        
+
+		<!-- Start Search Popup -->
+		<div class="box-search-content search_active block-bg close__top">
+			<form id="search_mini_form" class="minisearch" action="#">
+				<div class="field__search">
+					<input type="text" placeholder="Search entire store here...">
+					<div class="action">
+						<a href="#"><i class="zmdi zmdi-search"></i></a>
+					</div>
+				</div>
+			</form>
+			<div class="close__wrap">
+				<span>close</span>
+			</div>
+		</div>
+		<!-- End Search Popup -->
